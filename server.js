38cb2aa421b7dc05abe1a3dadc30fd116eb1a52c
wa1ead <waliddraa2@gmail.com>
2024-06-26 13:35:44 +0100

    Items route: backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const Items = require("./routes/api/Items");
 
 const app = express();
 
@@ -15,6 +16,9 @@ mongoose
   .connect(db)
   .then(() => console.log("MongoDb connected..."))
   .catch((err) => console.log(err));
+
+//GENERATE ROUTES
+app.use("/api/items", Items);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
