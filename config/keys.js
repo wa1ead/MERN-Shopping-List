6ab4a91720ad6fda04d1e497e2c0a41e42e471e1
wa1ead <waliddraa2@gmail.com>
2024-06-26 13:19:25 +0100

    MongoDb connect: backend

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://wa1ead:walid2002@list.mv1a8zx.mongodb.net/?retryWrites=true&w=majority&appName=list",
+};
--- server.js
+++ server.js
@@ -6,3 +6,15 @@ const app = express();
 
 //BODY-PARSER MIDDLEWARE
 app.use(bodyParser.json());
+
+//DB CONFIG
+const db = require("./config/keys").mongoURI;
+
+//CONNECT TO MONGODB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDb connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
