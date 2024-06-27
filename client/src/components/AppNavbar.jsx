e64b2d4e0484e5a38f7ee9c02c94c127699587fb
wa1ead <waliddraa2@gmail.com>
2024-06-27 13:25:04 +0100

    creating the Navbar Component: reactstrap

---
+++ client/src/components/AppNavbar.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+function AppNavbar(props) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <div>
+      <Navbar color="dark" dark expand="sm" className="mb-5">
+        <NavbarBrand href="/">ShoppingList</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="https://github.com/wa1ead">GitHub</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default AppNavbar;
