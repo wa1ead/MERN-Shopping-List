1dcbd89073dec31b059d22a36940a8505a624805
wa1ead <waliddraa2@gmail.com>
2024-07-02 13:03:07 +0100

    connect middleware: Axios

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 import itemReducer from "./features/item/itemSlice";
 
 const store = configureStore({
   reducer: { items: itemReducer },
-  middleware: [thunk],
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export default store;
