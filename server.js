c8828ca122c72eb4f9facd0a99b57f55d790c10d
wa1ead <waliddraa2@gmail.com>
2024-07-10 17:04:08 +0100

    configure for deployment: heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 const Items = require("./routes/api/Items");
 
 const app = express();
@@ -23,6 +24,15 @@ mongoose
 
 //GENERATE ROUTES
 app.use("/api/items", Items);
+
+//SERVE STATIC ASSETS
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
