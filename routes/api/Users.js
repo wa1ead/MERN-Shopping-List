bcc5d8c4b2476d335cd07337f9a9b8549bf22828
wa1ead <waliddraa2@gmail.com>
2024-07-15 16:16:40 +0100

    jwtSecret: JSON Web Token

--- routes/api/Users.js
+++ routes/api/Users.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const router = express.Router();
 const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
 
 //ITEM MODEL
 const User = require("../../models/User");
@@ -27,13 +29,25 @@ router.post("/", (req, res) => {
         if (err) throw err;
         newUser.password = hash;
         newUser.save().then((user) => {
-          res.json({
-            user: {
-              id: user.id,
-              name: user.name,
-              email: user.email,
+          jwt.sign(
+            { id: user.id },
+            config.get("jwtSecret"),
+            {
+              expiresIn: 3600,
             },
-          });
+            (err, token) => {
+              if (err) throw err;
+
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email,
+                },
+              });
+            }
+          );
         });
       });
     });
