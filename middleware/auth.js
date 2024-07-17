a2643f3c9fce7bdd8e2f6937d9a577ea4db94c2b
wa1ead <waliddraa2@gmail.com>
2024-07-17 13:20:46 +0100

    setup middleware: jwt

---
+++ middleware/auth.js
@@ -0,0 +1,20 @@
+const config = require("config");
+const jwt = require("jsonwebtoken");
+
+function auth(req, res, next) {
+  const token = res.header("x-auth-token");
+
+  if (!token) {
+    res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  try {
+    const docoded = jwt.verify(token, config.get("jwtSecret"));
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: e || "Token not valid" });
+  }
+}
+
+module.exports = auth;
