ff518625240be73afe8acc65841756545de789cd
wa1ead <waliddraa2@gmail.com>
2024-07-19 20:06:45 +0100

    create errorSlice: Redux Toolkit

---
+++ client/src/features/error/errorSlice.js
@@ -0,0 +1,29 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const initialState = {
+  msg: {},
+  status: null,
+  id: null,
+};
+
+const errorSlice = createSlice({
+  name: "error",
+  initialState,
+  reducers: {
+    //GET ERROR
+    getErrors: (state, action) => {
+      state.msg = action.payload.msg;
+      state.status = action.payload.status;
+      state.id = action.payload.id;
+    },
+
+    //CLEAR ERROR
+    clearErrors: (state) => {
+      state.msg = {};
+      state.status = null;
+      state.id = null;
+    },
+  },
+});
+
+export default errorSlice.reducer;
