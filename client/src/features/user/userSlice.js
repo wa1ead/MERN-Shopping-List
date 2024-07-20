c1620a95fd8942382106a09b364ae2e150d062ed
wa1ead <waliddraa2@gmail.com>
2024-07-20 17:14:55 +0100

    add userSlice actions: Redux Toolkit

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   token: localStorage.getItem("token"),
@@ -7,32 +8,49 @@ const initialState = {
   isLoading: null,
 };
 
+const loadUser = createAsyncThunk("user/loadUser", async (getState) => {
+  //GET TOKEN
+  const token = getState().auth.token;
+
+  //HEADERS
+  const config = {
+    headers: {
+      "Content-Type": "application",
+    },
+  };
+
+  //ADD TOKEN TO HEADERS
+  if (token) {
+    config.headers["x-auth-token"] = token;
+  }
+  await axios
+    .get("/api/auth/user", config)
+    .then((response) => response.data)
+    .catch((err) => {
+      err.response.data;
+    });
+});
+
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers: {
-    //LOADING USER
-    userLoading: (state) => {
+  extraReducers: (builder) => {
+    //CHECK LOGIN & AUTH USER
+    builder.addCase(loadUser.pending, (state) => {
       state.isLoading = true;
-    },
-
-    userLoaded: (state, action) => {
-      (state.user = action.payload),
-        (state.isAuthenticated = true),
-        (state.isLoading = false);
-    },
-
-    //USER REGISTRATION
-    userRegistred: (state, action) => {
-      action.payload, (state.isAuthenticated = true), (state.isLoading = false);
-    },
-
-    userNotRegistred: (state) => {
-      (state.token = null),
-        (state.user = null),
-        (state.isAuthenticated = false),
-        (state.isLoading = false);
-    },
+    }),
+      builder.addCase(loadUser.fulfilled, (state, action) => {
+        (state.user = action.payload),
+          (state.isAuthenticated = true),
+          (state.isLoading = false);
+      }),
+      builder.addCase(loadUser.rejected, (state) => {
+        localStorage.removeItem("token"),
+          (state.token = null),
+          (state.user = null),
+          (state.isAuthenticated = false),
+          (state.isLoading = false);
+      });
   },
 });
 
